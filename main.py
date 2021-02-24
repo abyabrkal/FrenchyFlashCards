6b3d6ffe6d41ca51f4c8b845bdbd7fa00e5af265
Aby Kal <abyabrkal@gmail.com>
2021-02-24 14:20:11 +0400

    Added 3sec timer for english translation.

--- main.py
+++ main.py
@@ -8,27 +8,39 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-
 data = pandas.read_csv('french_words.csv')
 to_learn = data.to_dict(orient="records")
-#data_dict = dict(data.values)
+
+current_card = {}
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=bcard_img)
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=fcard_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 window = Tk()
 window.title("FrenchyFlashCards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # --------  ROW 0 ---------
 canvas = Canvas(width=800, height=526)
 fcard_img = PhotoImage(file="images/card_front.png")
 bcard_img = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=fcard_img)
+canvas_image = canvas.create_image(400, 263, image=fcard_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
